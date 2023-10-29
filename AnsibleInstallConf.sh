1c9b5ad22f9435adb14ac73731e830d737815c0e
SCHLOSSER Guilhem <guilhemETkarine@hotmail.fr>
2023-10-29 17:19:31 +0100

    Update AnsibleInstallConf.sh
    Casse la tête

--- AnsibleInstallConf.sh
+++ AnsibleInstallConf.sh
@@ -9,6 +9,17 @@
 # Dernière mise à jour: 28/10/2023
 ######################################
 
+######################################
+# Nom du script:  AnsibleInstallConf.sh
+# Utilité: Ce script installe Ansible et configure la structure de répertoire conformément aux meilleures pratiques.
+# Utilisation: 
+#   - Assurez-vous que le script est exécutable avec : sudo chmod +x AnsibleInstallConf.sh
+#   - Exécutez le script avec : sudo -H ./AnsibleInstallConf.sh
+#   - L'option -H garantit que le répertoire du projet et l'environnement Ansible sont créés dans le répertoire de l'utilisateur qui exécute le script.
+# Auteur: Guilhem SCHLOSSER
+# Dernière mise à jour: 28/10/2023
+######################################
+
 #!/bin/bash
 
 # Function to detect the Linux distribution family
@@ -193,3 +204,4 @@ EOL
 done
 
 printf "All tasks have been completed. The script is finished.\n"
+
